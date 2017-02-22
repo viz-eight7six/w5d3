71c6995bb443c48e5e2200bd434dbded25acd315
App Academy Student <student@appacademy.io>
2017-02-22 15:47:35 -0800

    do recursion and enumerables for js intro

--- introJS.js
+++ introJS.js
@@ -44,3 +44,149 @@ console.log(my_transpose([
     [3, 4, 5],
     [6, 7, 8]
   ]))
+
+
+function myEach(array, callback){
+  for(let i = 0; i < array.length; i++){
+    callback(array[i])
+  }
+}
+
+Array.prototype.myEach = function (func) {
+  for(let i = 0; i < this.length; i++){
+    func(this[i]);
+  }
+  return this;
+}
+
+
+const NUMS = [1, 2, 3, 4, 5];
+NUMS.myEach((num) => {
+  console.log(`square of ${num} is ${num * num}`);
+});
+
+Array.prototype.myMap = function(func) {
+  let new_array = []
+  this.myEach(el => {
+    new_array.push(func(el))
+  })
+  return new_array;
+}
+
+console.log(NUMS.myMap(num => num + 1))
+
+Array.prototype.myInject = function(func) {
+  let acc = this[0]
+  this.slice(1,this.length).myEach(el => {
+    acc = func(acc, el)
+  })
+  return acc
+}
+
+console.log(NUMS.myInject((acc, el) => acc*el ))
+
+function bubbleSort(array){
+  let sorted = false
+  while (!sorted) {
+    for(let i = 0; i<array.length;i++){
+      if (i + 1 === array.length){
+        sorted = true
+      }
+      if (array[i] > array [i+1]) {
+        let temp = array[i]
+          array[i] = array[i+1]
+          array[i+1] = temp
+          break
+      }
+    }
+  }
+  return array
+}
+
+console.log(bubbleSort([3,2,1]))
+
+function substrings(string) {
+  let new_arr = []
+  for(let i = 0; i<string.length; i++) {
+    for(let j = i+1; j<string.length+1; j++) {
+      new_arr.push(string.slice(i,j))
+    }
+  }
+  return new_arr
+}
+
+console.log(substrings("cat"))
+
+function range(start, end){
+  if (end < start) {
+    return []
+  }
+  if (range(start+1, end) === []){
+      return [start]
+    }
+  else {
+    return [start].concat(range(start+1, end))
+  }
+}
+
+console.log(range(1,7))
+
+
+function exp(num, power){
+  if (power === 0) {
+    return 1
+  }
+  return num * exp(num, power-1)
+}
+
+console.log(exp(2,5))
+
+function fib(n){
+  if (n <= 2) {
+    let arr = [0,1]
+    return arr.slice(0,n)
+  }
+  return fib(n-1).concat([fib(n-1)[fib(n-1).length-1] + fib(n-2)[fib(n-2).length-1]])
+}
+
+console.log(fib(7))
+
+function bsearch(array, target){
+  let midpoint = Math.floor(array.length/2)
+  if (array.length === 1 && target!== array[midpoint]){
+    return null
+  }
+  if (target > array[midpoint]){
+    if (bsearch(array.slice(midpoint, array.length), target)){
+      return midpoint + bsearch(array.slice(midpoint, array.length), target)
+    }
+    return null
+  }
+  else if (target < array[midpoint]){
+    return bsearch(array.slice(0, midpoint), target)
+  }
+  else {
+    if (target === array[midpoint]) {
+      return midpoint
+    }
+    else {
+      return null
+    }
+  }
+}
+
+console.log(bsearch([1, 2, 3], 1))
+console.log(bsearch([2, 3, 4, 5], 3))
+console.log(bsearch([2, 4, 6, 8, 10], 6))
+console.log(bsearch([1, 3, 4, 5, 9], 5))
+console.log(bsearch([1, 2, 3, 4, 5, 6], 6))
+console.log(bsearch([1, 2, 3, 4, 5, 6], 0))
+console.log(bsearch([1, 2, 3, 4, 5, 7], 6))
+
+
+// function merge_sort(array){
+//   let midpoint = Math.floor(array.length/2)
+//   let left = array.slice(0, midpoint)
+//   let right = array.slice(midpoint, array.length)
+//
+// }
